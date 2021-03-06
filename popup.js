970b6eb1dcf0c96a38199b9cd73323aabae29ef2
reio <reiokai@gmil.com>
2021-03-07 01:16:38 +0800

    storage words

--- popup.js
+++ popup.js
@@ -1,2 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.local.get(['words'], 
+    result => {
+      const words = Object.keys(result.words);
+      document.getElementById('words-count').textContent = words.length;
+      document.getElementById('words').textContent = words.join(', ');
+    }
+  );
 });
